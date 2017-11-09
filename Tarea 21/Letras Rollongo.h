ce60a870694a3b80eeda6b1782dabced7fb740f3
raygadasbaezrodrigo <31478555+raygadasbaezrodrigo@users.noreply.github.com>
2017-11-09 00:08:12 -0600

    Add files via upload

---
+++ Tarea 21/Letras Rollongo.h
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+void fnR(){
+
+printf("RRRR\n");
+printf("R  R\n");
+printf("RRRR\n");
+printf("R R  \n");
+printf("R  R \n");
+}
+	void fnO(){
+	printf("OOOO\n");
+	printf("O  O\n");
+	printf("O  O\n");
+	printf("O  O\n");
+	printf("OOOO\n");
+	
+}
+void fnD(){
+	printf("DDD\n");
+	printf("D  D\n");
+	printf("D  D\n");
+	printf("D  D\n");
+	printf("DDD\n");
+	
+}
+void fnI(){
+
+	printf("IIII\n");
+	printf(" II \n");
+	printf(" II \n");
+	printf(" II \n");
+	printf("IIII\n");
+}
+void fnG()
+{
+	printf("GGGG\n");
+	printf("G   \n");
+	printf("G GG\n");
+	printf("G  G\n");
+	printf("GGGG\n");
+}
+
---
+++ Tarea 21/Nombre con Funciones 21.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"Letras Rollongo.h"
+int main ()
+{
+
+	fnR();
+	printf("\n");
+	fnO();
+		printf("\n");
+	fnD();
+		printf("\n");
+	fnR();
+		printf("\n");
+	fnI();
+		printf("\n");
+	fnG();
+		printf("\n");
+	fnO();
+}
+	
